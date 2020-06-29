8a900e6459afda4104f0838dbf32fea7aa87eecd
Sankar <sankarck.2@gmail.com>
2020-06-29 21:04:25 +0530

    Music app admin page

---
+++ examples/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="../assets/css/login.css" rel="stylesheet" />
+
+</head>
+<body>
+<div class="login">
+  <h1>Login to Web App</h1>
+  <form method="post" action="">
+    <p><input type="text" name="login" value="" placeholder="Username or Email"></p>
+    <p><input type="password" name="password" value="" placeholder="Password"></p>
+    <p class="remember_me">
+    </p>
+    <p class="submit"><input type="submit" name="commit" value="Login"></p>
+  </form>
+</div>
+
+</body>
+</html>
